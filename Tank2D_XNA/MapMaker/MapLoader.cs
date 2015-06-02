c8dd290588f33f6dd55bd80cde962f5263f760cd
AlexNav73 <fifthhorseman@mail.ru>
2015-06-02 17:59:51 +0300

    Fix bug with empty file name

--- Tank2D_XNA/MapMaker/MapLoader.cs
+++ Tank2D_XNA/MapMaker/MapLoader.cs
@@ -17,27 +17,28 @@ namespace MapMaker
         public MapLoader(string fileName)
         {
             _entity = new List<EntityInfo>();
+            if (String.IsNullOrEmpty(fileName)) return;
 
             try
             {
                 using (FileStream fs = File.Open(fileName, FileMode.Open))
-                using (XmlReader reader = XmlReader.Create(fs))
-                {
-                    while (reader.ReadToFollowing("Entity"))
+                    using (XmlReader reader = XmlReader.Create(fs))
                     {
-                        EntityInfo tmp = new EntityInfo();
-                        reader.MoveToFirstAttribute();
-                        tmp.Type = reader.GetAttribute("type");
-                        tmp.TankType = reader.GetAttribute("class");
+                        while (reader.ReadToFollowing("Entity"))
+                        {
+                            EntityInfo tmp = new EntityInfo();
+                            reader.MoveToFirstAttribute();
+                            tmp.Type = reader.GetAttribute("type");
+                            tmp.TankType = reader.GetAttribute("class");
 
-                        int x, y;
-                        Int32.TryParse(reader.GetAttribute("x"), out x);
-                        Int32.TryParse(reader.GetAttribute("y"), out y);
+                            int x, y;
+                            Int32.TryParse(reader.GetAttribute("x"), out x);
+                            Int32.TryParse(reader.GetAttribute("y"), out y);
 
-                        tmp.Position = new Point(x, y);
-                        _entity.Add(tmp);
+                            tmp.Position = new Point(x, y);
+                            _entity.Add(tmp);
+                        }
                     }
-                }
             }
             catch (IOException)
             {
